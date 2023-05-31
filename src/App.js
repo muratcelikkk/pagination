646d59ab6e92331fda9fcff06a7655cbcce514d0
murat <murat.tc2014@gmail.com>
2023-05-31 11:34:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [items, seItems] = useState([]);
+  const [visible,setVisible] =useState(3);
+
+  function showMoreMeals(){
+    setVisible(item=>item += 3)
+  }
+    
+  
+
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=british")
+    .then((res)=>res.json())
+    .then((data)=>seItems(data.meals))
+  }, [])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header">British Meals <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Flag_of_the_United_Kingdom_%281-2%29.svg/1200px-Flag_of_the_United_Kingdom_%281-2%29.svg.png" alt="img" /></h1>
+      <div className="container">
+        {items.slice(0,visible).map(item=>(
+          <div className='card'>
+            <div className="image">
+              <img src={item.strMealThumb} alt="img" />
+            </div>
+            <p>{item.strMeal}</p>
+          </div>
+        ))}
+        <button onClick={showMoreMeals}>Click for More Meals</button>
+      </div>
     </div>
   );
 }
